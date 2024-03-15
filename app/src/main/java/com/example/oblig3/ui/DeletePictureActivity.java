bf6cb375e4c3d4e15ab47094487847e0e00a0d65
MarHan123 <m_hansen1993@hotmail.com>
2024-03-15 17:39:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/oblig3/ui/DeletePictureActivity.java
@@ -0,0 +1,17 @@
+package com.example.oblig3.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.oblig3.R;
+
+public class DeletePictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_picture);
+
+    }
+}
